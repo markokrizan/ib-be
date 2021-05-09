d995ee610c9c0cfe3d6f710319bd62eff8d54feb
Marko Krizan <markokrizan64@gmail.com>
2021-05-09 19:27:27 +0200

    Move user controller logic to user service

--- src/main/java/rs/ac/uns/ftn/clinic/controller/UserController.java
+++ src/main/java/rs/ac/uns/ftn/clinic/controller/UserController.java
@@ -1,10 +1,9 @@
 package rs.ac.uns.ftn.clinic.controller;
 
-import rs.ac.uns.ftn.clinic.exception.ResourceNotFoundException;
 import rs.ac.uns.ftn.clinic.model.User;
 import rs.ac.uns.ftn.clinic.payload.*;
-import rs.ac.uns.ftn.clinic.repository.UserRepository;
 import rs.ac.uns.ftn.clinic.security.UserPrincipal;
+import rs.ac.uns.ftn.clinic.service.UserService;
 import rs.ac.uns.ftn.clinic.security.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,31 +16,27 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/users/me")
     @PreAuthorize("hasRole('USER')")
     public User getCurrentUser(@CurrentUser UserPrincipal currentUser) {
-        return userRepository.findById(currentUser.getId()).orElse(null);
+        return userService.getUserById(currentUser.getId());
     }
 
     @GetMapping("/users")
     @PreAuthorize("hasAuthority('ALL_USER_READ_PRIVILEGE')")
     public Page<User> getAllUsers(Pageable pageable) {
-        return userRepository.findAll(pageable);
+        return userService.getAllUsers(pageable);
     }
 
     @GetMapping("/users/checkUsernameAvailability")
     public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
-        Boolean isAvailable = !userRepository.existsByUsername(username);
-        return new UserIdentityAvailability(isAvailable);
+        return userService.checkUsernameAvailability(username);
     }
 
     @GetMapping("/users/{username}")
     public User getUserProfile(@PathVariable(value = "username") String username) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
-
-        return user;
+        return userService.getUserByUsername(username);
     }
 }
--- src/main/java/rs/ac/uns/ftn/clinic/service/UserService.java
+++ src/main/java/rs/ac/uns/ftn/clinic/service/UserService.java
@@ -4,13 +4,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import rs.ac.uns.ftn.clinic.exception.AppException;
+import rs.ac.uns.ftn.clinic.exception.ResourceNotFoundException;
 import rs.ac.uns.ftn.clinic.model.User;
+import rs.ac.uns.ftn.clinic.payload.UserIdentityAvailability;
 import rs.ac.uns.ftn.clinic.repository.UserRepository;
 import rs.ac.uns.ftn.clinic.security.JwtTokenProvider;
 
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -54,5 +58,23 @@ public class UserService {
                 .buildAndExpand(result.getUsername()).toUri();
 
         return location;
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+    }
+
+    public Page<User> getAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    public UserIdentityAvailability checkUsernameAvailability(String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
     }
 }
